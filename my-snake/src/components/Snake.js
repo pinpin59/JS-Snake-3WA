a68e81f6010494e4e50664ab968f2915bc283fae
TorresJules <70376196+TorresJules@users.noreply.github.com>
2022-12-16 12:08:05 +0100

    add séparation canvas et ctx

--- my-snake/src/components/Snake.js
+++ my-snake/src/components/Snake.js
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { setIsInit, setCanvas, randomNumbere } from "../actions/gameActions";
+import {clear} from "@testing-library/user-event/dist/clear";
 
 
 const Snake = () => {
@@ -8,29 +9,49 @@ const Snake = () => {
     const [canvas, setCanvas] = useState('')
     const [x , setX] = useState(0)
     const [y , setY] = useState(0)
+    const [start, setStart] = useState()
     const snake = useRef('snake')
+    const [ctx, setCtx] = useState('');
 
     const dispatch = useDispatch();
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            console.log('This will run every second!');
+        }, 3000);
+        return () => clearInterval(interval);
+    }, [setStart]);
+
+    //mouvements
     window.onkeydown = function(event) {
         let keyPr = event.keyCode;
-        console.log(keyPr);
 
+        const move = (direction, value) =>{
+            if (direction === "Y"){
+                return(
+                    setY(y + value),
+                    console.log(y)
+                )
+            }
+        }
         switch(keyPr){
-            case 38: 
+            case 38:
+                setStart('ok')
                 setY(y - 20)
+                // setInterval(move("Y", -20), 1000);
+                // const moveInterval = setInterval(console.log("okok"), 1000)
+                // return () => clearInterval(moveInterval)
+
                 break;
             case 40:
-                setY(y + 20)
+                // setY(y + 20)
+                setInterval(move("Y", 20), 1000);
                 break;
             default :
 
         }
         
-    }    
-    console.log('====================================');
-    console.log(y,x);
-    console.log('====================================');
+    }
 
     function randomNumber(){
         let number = (Math.floor(Math.random() * 78) + 1);
@@ -51,29 +72,38 @@ const Snake = () => {
     setCanvas(snake.current);
    },[])
 
+    //set CANVAS
+    useEffect(()=>{
+        if(canvas){
+            setCanvas(snake.current);
+            setCtx(canvas.getContext('2d'));
+        }
+
+    }, [canvas])
+
+   // set CTX
+    useEffect(()=>{
+        if (ctx){
+            setCtx(canvas.getContext('2d'));
+            ctx.beginPath();
+            ctx.fillStyle = "#789"
+            ctx.fillRect(randomNumber(),randomNumber2(),20,20)
+            ctx.beginPath();
+            ctx.fillStyle = "#333"
+            ctx.fillRect(0, 0, 800, 600)
+        }
+    })
 
    useEffect(() => {
     if(canvas){
-        
-        let ctx = canvas.getContext('2d');
-
-        ctx.beginPath();
-        ctx.fillStyle = "#333"
-        ctx.fillRect(0, 0, 800, 600)
-
-        ctx.beginPath();
-        ctx.fillStyle = "#789"
-        ctx.fillRect(randomNumber(),randomNumber2(),20,20)
 
         ctx.beginPath();
         ctx.fillStyle = "green"
-                    //Vers la D et G - Vers le H et B
-        ctx.fillRect(x,y,20,20)
-        console.log(ctx);
 
+        //Vers la D et G - Vers le H et B
+        ctx.fillRect(x,y,20,20)
     }
-    // console.log(isInit);
-   },[canvas, x ,y])
+   },[x ,y])
 
 
     return(
